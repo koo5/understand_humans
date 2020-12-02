d475a20d5b0652e32b45495220adb19b4934fb5b
koo5 <kolman.jindrich@gmail.com>
2020-12-02 20:23:01 +0100

    ...

---
+++ uh/scraps/code/inventory.py
@@ -0,0 +1,19 @@
+"""
+the program processess events sequentially, in chronological order, and for each event, produces a new state of the kb.
+
+
+
+put('cat toys', 'cat toys box')
+put('cat toys box', 'shelf 2')
+put('glycerin na bublifuk, nesterilni', 'krabice zelena mozzarella')
+
+
+
+
+
+
+
+
+
+
+"""
